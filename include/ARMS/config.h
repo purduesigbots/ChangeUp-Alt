a80aaa84528e63fbaecd03d915fae7b5560fd2c6
micahrassi <mrassi@purdue.edu>
2021-01-12 12:21:16 -0500

    Fix config file that was overwritten by the update

--- include/ARMS/config.h
+++ include/ARMS/config.h
@@ -4,16 +4,16 @@
 // Drivetrain configuration constants
 namespace chassis {
 // negative numbers mean reversed motor
-#define LEFT_MOTORS 1, 2
-#define RIGHT_MOTORS -3, -4
-#define GEARSET 200 // rpm of chassis motors
+#define LEFT_MOTORS -14, -13
+#define RIGHT_MOTORS 3, 1
+#define GEARSET 600 // rpm of chassis motors
 
-#define DISTANCE_CONSTANT 273 // ticks per distance unit
-#define DEGREE_CONSTANT 2.3   // ticks per degree
+#define DISTANCE_CONSTANT 41.45 // ticks per distance unit
+#define DEGREE_CONSTANT 2.3     // ticks per degree
 
 // chassis settling constants
-#define SETTLE_TIME 8
-#define SETTLE_THRESHOLD_LINEAR 3
+#define SETTLE_TIME 5
+#define SETTLE_THRESHOLD_LINEAR 1
 #define SETTLE_THRESHOLD_ANGULAR 1
 
 // slew control (autonomous only)
@@ -24,15 +24,16 @@ namespace chassis {
 #define IMU_PORT 0            // port 0 for disabled
 #define ENCODER_PORTS 0, 0, 0 // port 0 for disabled
 #define EXPANDER_PORT 0
+
 #define JOYSTICK_THRESHOLD 10 // min value needed for joystick to move drive
 } // namespace chassis
 
 namespace odom {
-#define ODOM_DEBUG 1
-#define LEFT_RIGHT_DISTANCE 6.375 // only needed for non-imu setups
-#define MIDDLE_DISTANCE 5.75      // only needed if using middle tracker
-#define LEFT_RIGHT_TPI 41.4       // Ticks per inch
-#define MIDDLE_TPI 41.4           // Ticks per inch
+#define ODOM_DEBUG 1              // debug print out
+#define LEFT_RIGHT_DISTANCE 7.625 // only needed for non-imu setups
+#define MIDDLE_DISTANCE 7.625     // only needed if using middle tracker
+#define LEFT_RIGHT_TPI 69.44      // Ticks per inch
+#define MIDDLE_TPI 69.44          // Ticks per inch
 #define SLEW_STEP 10              // point function slew
 #define HOLONOMIC 1               // holonomic chassis odom
 } // namespace odom
@@ -49,9 +50,9 @@ namespace pid {
 #define DIF_KP .5
 
 // odom point constants
-#define LINEAR_POINT_KP 8
+#define LINEAR_POINT_KP 4
 #define LINEAR_POINT_KD 0
-#define ANGULAR_POINT_KP 50
+#define ANGULAR_POINT_KP 35
 #define ANGULAR_POINT_KD 0
 #define MIN_ERROR 5 // minimum error, stops robot from spinning around point
 } // namespace pid
