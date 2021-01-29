c9f1c47c443b197ebf9571112c4db23aaa96bcfe
Mihir Laud <mihirlaud@gmail.com>
2021-01-28 20:31:08 -0500

    Add intake routine at the end of vision alignment

--- src/subsystems/vision.cpp
+++ src/subsystems/vision.cpp
@@ -1,3 +1,4 @@
+#include "ARMS/chassis.h"
 #include "main.h"
 
 const int target = 165;
@@ -22,6 +23,7 @@ void align() {
 
 	int pe = 0;
 	int count = 0;
+
 	while (true) {
 		int x_pos =
 		    sensor->get_by_sig(0, 1).x_middle_coord; // 0 = largest object, 1 = id
@@ -34,12 +36,21 @@ void align() {
 
 		if (count < minimum) {
 			count += 10;
-		} else if (chassis::settled) {
+		} else if (chassis::settled()) {
 			break;
 		}
 
 		delay(10);
 	}
+
+	intake::move(100);
+
+	while (sensor->get_by_sig(0, 1).top_coord < 180) {
+		chassis::tank(60, 60);
+	}
+
+	chassis::tank(0, 0);
+	intake::move(0);
 }
 
 } // namespace vision
