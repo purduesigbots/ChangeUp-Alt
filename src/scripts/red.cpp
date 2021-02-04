75fdeff340f8fe6f4423588716dddb3d50266ea7
Parsia Bahrami <parsiabahrami80@gmail.com>
2021-02-03 22:49:50 -0500

    further tuning of skills auton

--- src/scripts/red.cpp
+++ src/scripts/red.cpp
@@ -7,21 +7,23 @@ void red() {
 	indexer::move(100);
 	flywheel::move(-100);
 	delay(500);
-	indexer::move(0);
-	flywheel::move(0);
+	indexer::move(100);
+	flywheel::move(-10);
 	chassis::move(12);
-	chassis::turn(120);
-	odom::holoThru({22.6, 11.6}, 120);
-	// delay(50);
-	// chassis::fast(6, 40);
-	// chassis::voltage(10);
-	// delay(300);
-	// intake::move(100);
-	// indexer::move(100);
-	// macro::score();
-	// delay(1500);
-	// intake::move(0);
-	// delay(600);
+	odom::holo({14, 14}, 132);
+	indexer::move(50);
+	chassis::move(10);
+
+	delay(50);
+	chassis::fast(6, 40);
+	chassis::voltage(10);
+	delay(300);
+	intake::move(100);
+	indexer::move(100);
+	macro::score();
+	delay(1500);
+	intake::move(0);
+	delay(600);
 	/*
 	  // goal 1
 	  macro::intake();
