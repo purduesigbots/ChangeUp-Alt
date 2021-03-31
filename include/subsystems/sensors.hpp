b0d11975f14f52811a22dde4a889dd3df5f6307c
Mihir Laud <mihirlaud@gmail.com>
2021-03-30 21:33:36 -0400

    Test ultrasonic sensors

--- include/subsystems/sensors.hpp
+++ include/subsystems/sensors.hpp
@@ -9,6 +9,7 @@ bool detectRed();
 bool detectBlue();
 bool detectLine();
 int read_ul_side();
+void get_xy_offset(double target_angle);
 
 } // namespace sensors
 #endif
--- src/subsystems/sensors.cpp
+++ src/subsystems/sensors.cpp
@@ -1,4 +1,5 @@
 #include "sensors.hpp"
+#include "ARMS/chassis.h"
 #include "main.h"
 
 namespace sensors {
@@ -10,6 +11,8 @@ namespace sensors {
 Optical color(13);
 ADIAnalogIn line_sensor('a');
 ADIUltrasonic ul_side('c', 'd');
+ADIUltrasonic ul_left('g', 'h');
+ADIUltrasonic ul_right('e', 'f');
 
 void init() {
 	color.set_led_pwm(100);
@@ -29,6 +32,18 @@ bool detectLine() {
 
 int read_ul_side() {
 	return ul_side.get_value();
+}
+
+void get_xy_offset(double target_angle) {
+
+	double angle = fmod(chassis::angle(), 360);
+	double theta = fabs(angle - target_angle) * M_PI / 180;
+
+	double x = ul_right.get_value() / 25.4 * cos(theta);
+	double y = ul_left.get_value() / 25.4 * cos(theta);
+
+	printf("%d %d %lf %lf %lf\n", ul_right.get_value(), ul_left.get_value(), x, y,
+	       theta);
 }
 
 } // namespace sensors
